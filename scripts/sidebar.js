ad86d496c31852d01032581da2d4507fd531b5d2
Sarthak0008 <98695968+Sarthak0008@users.noreply.github.com>
2022-06-18 17:30:37 +0530

    side menu

---
+++ scripts/sidebar.js
@@ -0,0 +1,57 @@
+$('.feat-btn').click(function(){
+    $('#feat-show').toggleClass("show");
+  });
+  $('.feat-btn1').click(function(){
+    $('#feat-show1').toggleClass("show");
+  });
+  $('.feat-btn2').click(function(){
+    $('#feat-show2').toggleClass("show");
+  });
+  $('.feat-btn3').click(function(){
+    $('#feat-show3').toggleClass("show");
+  });
+  $('.feat-btn4').click(function(){
+    $('#feat-show4').toggleClass("show");
+  });
+  $('.feat-btn5').click(function(){
+    $('#feat-show5').toggleClass("show");
+  });
+  $('.feat-btn6').click(function(){
+    $('#feat-show6').toggleClass("show");
+  });
+  $('.feat-btn7').click(function(){
+    $('#feat-show7').toggleClass("show");
+  });
+  $('.feat-btn8').click(function(){
+    $('#feat-show8').toggleClass("show");
+  });
+  $('.feat-btn9').click(function(){
+    $('#feat-show9').toggleClass("show");
+  });
+  $('.feat-btn10').click(function(){
+    $('#feat-show10').toggleClass("show");
+  });
+  $('.feat-btn11').click(function(){
+    $('#feat-show11').toggleClass("show");
+  });
+  $('.feat-btn12').click(function(){
+    $('#feat-show12').toggleClass("show");
+  });
+  $('.feat-btn13').click(function(){
+    $('#feat-show13').toggleClass("show");
+  });
+
+
+  var data=document.querySelector("body")
+var x= document.querySelector(".cancelbtn");
+x.style.cursor="pointer";
+
+
+x.addEventListener("click",function(){
+   deleteItem(data)
+})
+function deleteItem(data){
+ 
+data.remove();
+ 
+}
