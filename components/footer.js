ea8aac8ba27edeaee8b193bd993c1f718466432c
SanketPaharia <sanket.paharia@gmail.com>
2022-06-18 15:06:40 +0530

    ftr

---
+++ components/footer.js
@@ -0,0 +1,457 @@
+function footer(){
+    return `
+    <div id="box_1"> 
+            <div id="all_list">
+                <div id="b1">
+                    <div class="nat">
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a  target="_blank" href="" >NATIONAL 
+                            <span class="fas fa-caret-down"></span>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="li1" style="list-style-type:none">
+                       
+                            <li>
+                                <a target="_blank" href="">Politics</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">North and Central</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">South</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">East and Northeast</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">West</a>
+                            </li> 
+                       
+                    </div>
+                </div>
+                
+                <div id="b2">
+                    <div>
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >KARNATAKA</a>
+                        </p>
+                    </div>
+                    <div class="li2" style="list-style-type:none">
+                        
+                            <li>
+                                <a target="_blank" href="">Top Stories</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Politics</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Districts</a>
+                            </li>
+                        
+                    </div>
+                </div>
+
+                <div id="b3">
+                    <div >
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >BENGALURU</a>
+                        </p>
+                    </div>
+                    <div class="li3" style="list-style-type:none">
+                       
+                            <li >
+                                <a  target="_blank" href="">Top Stories</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Bengaluru Crime</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Politics</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Infrastructure</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Life in the City</a>
+                            </li>
+                       
+                    </div>
+                </div>
+
+                <div id="b4">
+                    <div>
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >SPORTS</a>
+                        </p>
+                    </div>
+                    <div class="li4" style="list-style-type:none">
+                        
+                            <li>
+                                <a target="_blank" href="">Cricket</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Formula 1 with DH</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Football</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Tennis</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Sportsscene</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Other Sports</a>
+                            </li>
+                        
+                    </div>
+                </div>
+
+                <div id="b5">
+                    <div>
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >BUSSINESS</a>
+                        </p>
+                    </div>
+                    <div class="li5" style="list-style-type:none">
+                        
+                            <li>
+                                <a target="_blank" href="">Bussiness News</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Family Finance</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Technology</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">DH wheels</a>
+                            </li>
+                           
+                       
+                    </div>
+
+                </div>
+
+                <div id="b6">
+                    <div>
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >MULTIMEDIA</a>
+                        </p>
+                    </div>
+                    <div class=li6 style="list-style-type:none">
+                        
+                            <li>
+                                <a target="_blank" href="">Photos</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Videos</a>
+                            </li>
+                            
+                        
+                    </div>
+                </div>
+
+                <div id="b7">
+                    <div >
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >OPINION</a>
+                        </p>
+                    </div>
+                    <div class="li7" style="list-style-type:none">
+                            <li>
+                                <a target="_blank" href="">DH Views</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Editorials</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Panorama</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Comment</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">In Perspective</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Right in the Middle</a>
+                            </li>
+                            
+                       
+                    </div>
+                </div>
+
+                <div id="b8">
+                    <div>
+                        <p style="border-bottom: 1px solid rgb(150, 181, 181) ;font-weight: bolder">
+                            <a target="_blank" href="" >ENTERTAINMENT</a>
+                        </p>
+                    </div>
+                    <div class="li8" style="list-style-type:none">
+                        
+                            <li>
+                                <a target="_blank" href="">Entertainment News</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">DH Showtime</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">DH Taklies</a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="">Arts, Books & Culture</a>
+                            </li>
+                            
+                     
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div id="latest_news">
+                <div>
+                    <a target="_blank" href="">US FED'S BIGGEST INTEREST RATE HIKE SINCE 1994</a>
+                </div>
+                <div>
+                    <a target="_blank" href="">OPEN HUNTS FOR PRESIDENTIAL CANDIDATE</a>
+                </div>
+                <div>
+                    <a target="_blank" href="">CENTRE TRIES TO DOUSE AGNIVEER FIRE</a>
+                </div>
+                <div>
+                    <a target="_blank" href="">COAL BACK IN FOCUS</a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div id="box_2">
+            <div id="bx1">
+                <div>
+                    <p style="border-bottom: 1px solid grey ;font-weight: bolder;">
+                        DH PICKS
+                    </p>
+                </div>
+                <div style="list-style-type:none ;margin-top: px;">
+                  
+                        <li>
+                            <a target="_blank" href="">Trains set ablaze in Bihar amid protests over Agnipath</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Bihar becomes Agnipath over Modi's new hiring scheme</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Political significance of UP’s bulldozer demolitions</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Who’s got it right on EVs: Musk or Buffett?</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Fed-triggered recession in US may cost Biden a 2nd term</a>
+                        </li>
+                        
+                    
+                </div>
+            </div>
+            <div id="bx2">
+                <div>
+                    <p style="border-bottom: 1px solid grey ;font-weight: bolder;">
+                        LATEST STORIES
+                    </p>
+                </div>
+                <div style="list-style-type:none ;">
+                   
+                        <li>
+                            <a target="_blank" href="">Amazon pulled the IPL plug but Bharat bet still strong</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">ATF prices hiked by 16%; touch new record</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Delhi sees over 7,000 Covid cases in last 10 days</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Toshiba, Sony lose court fight against EU cartel fine</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Tech startups accelerate hiring of women</a>
+                        </li>
+                        
+                    
+                </div>
+
+            </div>
+
+
+            <div id="bx3" style="border-right: 1px solid grey" >
+                <div style="width: 90%">
+                    <p style="border-bottom: 1px solid grey; font-weight: bolder;">
+                        TRENDING NEWS
+                    </p>
+                </div>
+                <div style="list-style-type:none">
+                    
+                        <li>
+                            <a target="_blank" href="">People gather in support of Nupur Sharma in Ahmedabad</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Supa Dam goes dry, opens floodgates of memories</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Inspired by Tirumala, Iskcon builds temple in Bengaluru</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">31 students at 2 Bengaluru schools test Covid positive</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Bengaluru sees 3-fold rise in Covid cases</a>
+                        </li>
+                        
+                    
+                </div>
+            </div>
+
+
+            <div id="bx4">
+                <div>
+                    <div>
+                        <p style="border-bottom: 1px solid grey ;font-weight: bolder;">
+                           DOWNLOAD DH APP
+                        </p>
+                    </div>
+                    <div id="first_logo" >
+                        
+                                <div style="color: grey"><i class="fa fa-android" ></i></div>
+                                <div style="color:grey"><i class="fa-brands fa-apple"></i></div>
+                          
+                    </div>
+                </div>
+
+                <div>
+                    <div >
+                        <p style="border-bottom: 1px solid grey ;font-weight: bolder;">
+                            SERVICES
+                        </p>
+            
+                    </div>
+                    <div style="list-style-type:none ; display: flex; gap: 5px; flex-flow: row wrap; " >
+                        <li>
+                            <a target="_blank" href="">About</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Epaper</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Archives</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Sitemap</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Classifieds</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Contact</a>
+                        </li>
+                       
+                        
+                    </div>
+                </div>
+
+                
+                
+                <div>
+                    <div>
+                        <p style="border-bottom: 1px solid grey ;font-weight: bolder;">
+                            OUR GROUP SITES
+                        </p>
+            
+                    </div>
+                    <div  style="list-style-type:none ; display: flex; gap: 5px; flex-flow: row wrap;" >
+                        <li>
+                            <a target="_blank" href="">Printers Mysore</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Prajavani</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Sudha</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Mayura</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="">Exam Masermind</a>
+                        </li>
+                        
+                        
+                    </div>
+                </div>
+
+                <div id="pp" style="list-style-type:none ; font-weight: bol; margin-top: 10px;  display: flex; gap: 2px; flex-flow: row wrap;" >
+                   
+                    
+                    <li  >
+                        <a target="_blank" href="">Privacy Policy</a>
+                        
+                    </li>
+                    <li>
+                        <a target="_blank" href=""></a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="">Terms    </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href=""></a>
+                    </li>
+                    <li>
+                        <a target="_blank" href=""> Disclaimer</a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="">  </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="">Grievance      Redressal</a>
+                    </li>
+                   
+                    
+                </div>
+            </div>
+        </div>
+
+
+        <div id="box_3" style="justify-content:space-between ;display:flex">
+
+            <div style="white-space: nowrap;">
+                <p> © 2022 The Printers (Mysore) Private Ltd.</p>
+            </div>
+
+            <div id="icondiv" style="display:flex ; gap: 10px; padding-top: 10px ; " >
+
+
+               <!--  <div style="color:white ;font-size: 16px"><i class="fa fa-facebook" aria-hidden="true"></i></div>     
+                <div style="color: white ;font-size: 14px;"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+                <div style="color:white ;font-size: 14px;"><i class="fa fa-instagram"></i></div>
+                <div style="color:white ;font-size: 14px;"><i class="fa fa-youtube"></i></div>
+              -->
+             
+             
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-instagram"></i>
+                <i class="fa fa-youtube"></i>
+          
+              </div>
+        </div>
+    
+    
+    `
+}
+
+
+export default footer() ;
